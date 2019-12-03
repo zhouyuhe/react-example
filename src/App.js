de787459d7b142c756d484635e188d72c8c16e42
zhouyuhe <yuhe.zhou@weareadaptive.com>
2019-12-03 16:57:03 +0000

    timer work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const [second, setSecond]= useState(0)
+  const [isRunning, setIsRunning]=useState(false)
+  const toggleButton = ()=>{
+    //onClick and change the state dynamically
+    setIsRunning(!isRunning)
+  }
+  const reset=()=>{
+    //reset the current state, not the initial one
+    setSecond(0)
+    setIsRunning(false)
+  }
+  
+  useEffect(()=>{
+    //render only interval and second changes
+    let interval
+    if (isRunning){
+      interval=setInterval(() => {
+        setSecond(second+1)
+      }, 10);
+    }else if (!isRunning && second !==0){
+      clearInterval(interval)
+    }
+    return  () => clearInterval(interval)
+
+  }, [isRunning, second])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Timer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{second}</div>
+        <button onClick={reset}>Reset</button>
+        {/* isRunning condition to change it automatically */}
+        <button onClick={toggleButton}>{isRunning ? 'Pause':'Start'}</button>
       </header>
     </div>
   );
